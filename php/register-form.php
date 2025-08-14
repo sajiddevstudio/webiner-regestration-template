07b421e46fecd2c0c78627bfdd089d6842df93b2
Sajid Howladar <sajidalhasan402@gmail.com>
2025-08-14 23:21:23 +0600

    Add files via upload

---
+++ php/register-form.php
@@ -0,0 +1,35 @@
+<?php
+
+// if the url field is empty
+if(isset($_POST['url']) && $_POST['url'] == ''){
+
+	// put your email address here
+	$youremail = 'yourname@mail.com';  
+
+	// prepare message 
+	$body = "You have got a new message from the contact form on your website - Waybinar :
+	
+	Name:  $_POST[name]
+	Email:  $_POST[email]
+	Phone:  $_POST[phone]
+	Company:  $_POST[company]";
+
+	if( $_POST['email'] && !preg_match( "/[\r\n]/", $_POST['email']) ) {
+	  $headers = "From: $_POST[email]";
+	} else {
+	  $headers = "From: $youremail";
+	}
+
+	mail($youremail, 'Message from Waybinar', $body, $headers );
+
+} ?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Thanks!</title>
+</head>
+<body>
+<p> Thank you! We will get back to you soon.</p>
+</body>
+</html>
